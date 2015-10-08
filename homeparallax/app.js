35998b2bc7a31d2c31b1e12e4e4ad3da3037e7bf
Tom <tom.howe@bbc.co.uk>
2015-10-08 15:52:41 +0100

    cleaned up code

--- homeparallax/app.js
+++ homeparallax/app.js
@@ -1,14 +1,3 @@
-/*
-   var superScreen = new Layer({
-   x:0, y:0,
-   width: 1440,
-   height: 2392,
-   backgroundColor: "transparent"
-   });
-//superScreen.center();
-
-this.drawerPager.on(Events.DirectionLockDidStart, function(event, layer) {  scrolling = true; });
-*/
 var screenWidth = 1080;
 var smallHeight = 432;
 var smallHeight = 300;
@@ -43,7 +32,6 @@ var Item = function(imgSrc) {
         superLayer: this.layer
     });
     this.label.html = "<h1>"+pages.length + "</h1>";
-    //pager.addPage(this.layer,"bottom");
     this.layer.superLayer = pagerContent;
 }
 
@@ -74,83 +62,28 @@ pagerContent.draggable.constraints = {
     height: 2*pagerContent.height-1700};
 
 var dragCallback = function(event,layer){
-    //pager.content.on(Events.Move,function(event,layer){
 
-
-    var ratio = -layer.y/smallHeight;
+    var ratio = -layer.y/smallHeight; // how squished up the bottom elements are compared to the current element
     var currIndex = Math.floor(ratio);
     var interp = ratio % 1;
     //console.log("y" + layer.y + " index: "+currIndex + " interp " + interp);
 
     for (var i =0; i<pages.length; i++) {
-        //pages[i].layer.y = layer.y;
-
-            //pages[i].layer.x = 100;
-        if (i<currIndex) {
-            // off the top
+        if (i<=currIndex) {
+            // off the top, or current card
             pages[i].layer.y = smallHeight*i;
-        } else if (i==currIndex) {
-            //current card
-            pages[i].layer.y = smallHeight*i;
-            //pages[i].layer.x = 40;
         } else if (i==currIndex+1) {
             //next card
             pages[i].layer.y = (1-interp)*largeHeight + smallHeight*(i);
-            //pages[i].layer.x = 80;
         } else {
-            //pages[i].layer.y = largeHeight + smallHeight*(currIndex-i);
             //queued card
             pages[i].layer.y = largeHeight + smallHeight*(i);
-            //pages[i].layer.x = 120;
         }
     }
 
     if (currIndex<0) pages[0].layer.y=0;
 };
 
-//pager.content.on("change:y",function(event,layer){
 pagerContent.on("change:y",dragCallback);
 dragCallback(0,pagerContent);
-/*
 
-pager.content.on(Events.Move,function(event,layer){
-console.log("move!");
-});
-*/
-// The below code almost works...
-/*
-pagerContent.on(Events.DragEnd,function(event,layer){
-    console.log("drag end!");
-
-
-    console.log(pagerContent.y);
-
-        var ratio = -pagerContent.y/smallHeight;
-        console.log(ratio);
-        console.log(smallHeight);
-        var currIndex = Math.round(ratio);
-        if (currIndex<0) currIndex = 0;
-        console.log(currIndex);
-        var destY = -pages[currIndex].layer.y
-    console.log("desty is "+destY);
-       pagerContent.animate({
-       properties: { y: destY},
-       time:0.5
-       });
-
-    console.log("set the animation!");
-});
-*/
-
-
-/*
-   pager.on(Events.DragAnimationDidStart,function(event,layer){
-    console.log("drag anim started!");
-});
-
-pager.on(Events.DragAnimationDidEnd,function(event,layer){
-    console.log("drag anim ended!");
-});
-
-//pager.content.on(Events.Move,dragCallback2);
-*/
